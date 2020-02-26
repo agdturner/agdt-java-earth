0611d63be1a145cebfb29a338ee5f253ba1148d8
Andy Turner <A.G.D.Turner@leeds.ac.uk>
2020-02-26 00:12:29 +0000

    init

---
+++ src/main/java/uk/ac/leeds/ccg/projects/earth/io/E_Files.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2019 Centre for Computational Geography, University of Leeds.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.ac.leeds.ccg.projects.earth.io;
+
+import java.io.IOException;
+import uk.ac.leeds.ccg.generic.io.Generic_Defaults;
+import uk.ac.leeds.ccg.generic.io.Generic_Files;
+
+/**
+ * E_Files
+ * 
+ * @author Andy Turner
+ * @version 1.0.0
+ */
+public class E_Files extends Generic_Files {
+
+    private static final long serialVersionUID = 1L;
+    
+    public E_Files(Generic_Defaults d) throws IOException{
+        super(d);
+    }
+}
---
+++ src/main/java/uk/ac/leeds/ccg/projects/earth/process/E_Main.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2019 Centre for Computational Geography, University of Leeds.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.ac.leeds.ccg.projects.earth.process;
+
+import java.io.IOException;
+import uk.ac.leeds.ccg.generic.core.Generic_Environment;
+import uk.ac.leeds.ccg.generic.io.Generic_Defaults;
+import uk.ac.leeds.ccg.grids.core.Grids_Environment;
+import uk.ac.leeds.ccg.grids.process.Grids_ProcessorDEM;
+
+/**
+ * For running a model of Earth.
+ *
+ * @author Andy Turner
+ * @version 1.0.0
+ */
+public class E_Main extends Grids_ProcessorDEM {
+
+    private static final long serialVersionUID = 1L;
+
+    public E_Main(Grids_Environment env) throws IOException, Exception {
+        super(env);
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            E_Main t = new E_Main(new Grids_Environment(
+                    new Generic_Environment(new Generic_Defaults())));
+            t.run();
+        } catch (Exception ex) {
+            ex.printStackTrace(System.err);
+        }
+    }
+
+    public void run() {
+        try {
+        } catch (Exception | Error e) {
+            e.printStackTrace(System.err);
+        }
+    }
+
+}
